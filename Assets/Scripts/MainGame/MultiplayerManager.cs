f186986db9c2f09ff0e6eefc86709f483a3ac384
Griffin Rubin <17rubin@gmail.com>
2021-06-23 16:08:45 -0700

    reconnection testing

--- Assets/Scripts/MainGame/MultiplayerManager.cs
+++ Assets/Scripts/MainGame/MultiplayerManager.cs
@@ -161,7 +161,7 @@ public class MultiplayerManager : MonoBehaviourPunCallbacks, IPunObservable
                     if (!gameHistory.Contains(command))
                     {
                         Debug.Log("MISSING COMMAND: " + command);
-                        this.photonView.RPC("updateGameHistory", RpcTarget.All, command, actorNumber);
+                        this.photonView.RPC("updateGameHistory", RpcTarget.All, command, PhotonNetwork.MasterClient.ActorNumber);
                     }
                 }
             } else
